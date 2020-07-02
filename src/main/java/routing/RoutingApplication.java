de280c145cfbd0f3556e9318c6a95fa76a4d3520
TanskiV <tanskiyv@gmail.com>
2020-07-02 10:31:42 +0300

    with main filter

--- src/main/java/routing/RoutingApplication.java
+++ src/main/java/routing/RoutingApplication.java
@@ -3,15 +3,20 @@ package routing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import routing.filters.TokenFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class RoutingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoutingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoutingApplication.class, args);
+    }
 
-
+    @Bean
+    public TokenFilter tokenFilter() {
+        return new TokenFilter();
+    }
 
 }
---
+++ src/main/java/routing/filters/TokenFilter.java
@@ -0,0 +1,65 @@
+package routing.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenFilter extends ZuulFilter {
+    private static final String PATH_LOGIN  = "/account/{lang}/v1/login";
+    private static final String PATH_REGISTRATION = "/account/{lang}/v1";
+    private static final String PATH_VALIDATION  = "https://propetsaccountin.herokuapp.com/account/{lang}/v1/validation/update";
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String [] tempPath = request.getServletPath().split("/");
+        String path = tempPath[tempPath.length];
+        if (request.getMethod().equals(RequestMethod.PUT) && path.equals(PATH_LOGIN) || path.equals(PATH_REGISTRATION)){
+            return null;
+        }
+
+        String token = request.getHeader("X-Token");
+        if (token == null){
+            throw new ZuulException("Request with X-Token", 404, "Request with X-Token");
+        }
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Token", token);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(PATH_VALIDATION);
+        RequestEntity<String> tokenRequest = new RequestEntity<>(headers, HttpMethod.PUT, uriBuilder.build().toUri());
+        ResponseEntity<String> response =
+                restTemplate.exchange(tokenRequest, String.class);
+        boolean valid = Boolean.getBoolean(response.getHeaders().getFirst("Valid"));
+        if (!valid){
+            throw new ZuulException("Request with X-Token", 404, "Token not a valid");
+        }
+        token = response.getHeaders().getFirst("X-Token");
+        ctx.addZuulRequestHeader("X-Token", token);
+        return null;
+    }
+}
